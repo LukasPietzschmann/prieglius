edd712dd5f424f3e484d9841951816173ab68498
Lukas Pietzschmann <lukas.pietzschmann@uni-ulm.de>
2025-07-01 10:46:21 +0200

    Added GithubPackages as publishing target

--- Prieglius/build.gradle.kts
+++ Prieglius/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `java-library`
+    `maven-publish`
 }
 
 repositories {
@@ -12,5 +13,18 @@ dependencies {
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/LukasPietzschmann/prieglius")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
     }
 }
